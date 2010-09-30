f08206fcefd00ce377e9c2e79afc5af75ab17fec
Daniel Pfeifer <daniel@pfeifer-mail.de>
2010-09-30 11:47:23 +0200

    first commit

---
+++ Maoni.cmake
@@ -0,0 +1,16 @@
+
+MAONI_MODULE(Texturing
+  SOURCE
+    src/DGLBumpMapping.cpp
+    src/MultiTextureMapping.cpp
+    src/OzoneBumpMapping.cpp
+    src/SwiftlessBumpMapping.cpp
+    src/TextureMapping.cpp
+  DATA
+    data/crate.jpg
+    data/dgl_color_map.jpg
+    data/dgl_normal_map.jpg
+    data/l3d.jpg
+    data/ozone_color_map.jpg
+    data/ozone_normal_map.jpg
+  )
